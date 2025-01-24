b6a427787df034488ae3ca9d83cbb5817f15b1a8
Himansu Pokhariya <pokhariyahimansu@gmail.com>
2025-01-24 10:51:23 +0530

    modified

--- src/components /Header.js
+++ src/components /Header.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import './Header.css'; // Import styles
+import React from "react";
+import "./Header.css";
+import { FaInstagram, FaWhatsapp, FaFacebook, FaPhone } from "react-icons/fa";
 
-function Header() {
+const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">Artistry in Every Detail</div>
-      <nav>
-        <a href="#our-story">Our Story</a>
-        <a href="#shop">Shop</a>
-        <a href="#categories">Categories</a>
-        <a href="#blogs">Blogs</a>
-        <a href="#contact">Contact us</a>
-      </nav>
-      <div className="social-icons">
-        {/* Add social icons here */}
-        <i className="icon-facebook"></i>
-        <i className="icon-twitter"></i>
+    <header className="header"> 
+      <div className="header-top"> 
+        <div className="logo">
+          <img
+            src="/images/sac2.svg" 
+            alt="Shilpsagar Logo" 
+            className="logo-image" 
+          /> 
+        </div>
+        <div class="logo abc" > <h2 className="tagline">The art of becoming, not being...</h2> </div>
+        <div class="logo" ></div>
       </div>
+      <nav className="navbar">
+        <div ></div>
+        <ul className="nav-links">
+          <li>
+            <a href="#">Our Story</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+          <li>
+            <a href="#">Blogs</a>
+          </li>
+          <li>
+            <a href="#">Contact us</a>
+          </li>
+        </ul>
+        <div className="social-icons">
+          <a href="#" title="Instagram">
+            <FaInstagram />
+          </a>
+          <a href="#" title="WhatsApp">
+            <FaWhatsapp />
+          </a>
+          <a href="#" title="Facebook">
+            <FaFacebook />
+          </a>
+          <a href="#" title="Phone">
+            <FaPhone />
+          </a>
+        </div>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
