7d686b869c30083bd8c86785ca3fad8c028ff590
karan singh kanyal <119814521+karankanyal@users.noreply.github.com>
2025-03-19 11:45:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['65tss5-5173.csb.app', 'localhost', 'your-other-host.com'], // Add the host here
+  },
 });
